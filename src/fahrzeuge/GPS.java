72602b1407a889515b53d44d05598a41c84deb3d
Wladi <wladislaw.schmidt14@gmail.com>
2022-01-13 17:34:42 +0100

    added fahrzeuge

---
+++ src/fahrzeuge/GPS.java
@@ -0,0 +1,12 @@
+package fahrzeuge;
+
+public class GPS {
+    float laengengrad;
+    float breitengrad;
+
+    public GPS(float laengengrad, float breitengrad) {
+        this.laengengrad = laengengrad;
+        this.breitengrad = breitengrad;
+    }
+
+}
---
+++ src/fahrzeuge/Kfz.java
@@ -0,0 +1,46 @@
+package fahrzeuge;
+
+public abstract class Kfz {
+
+    int anzahlSitze;
+    int aktuellErhaltenePersonen;
+    int tankGroesse;
+    double tankinhalt;
+    int aktuelleGeschwindigkeit;
+    int maximaleGeschwindigkeit;
+    float anschaffungskosten;
+    GPS aktuelleGPSPosition;
+    boolean motorstand;
+
+    public void bremsen() {
+
+    }
+
+    public void beschleuinigen (){
+
+    }
+
+    public void fahren(GPS gpsPosition) {
+
+    }
+
+    public void aussteigen() {
+
+    }
+
+    public void tanken () {
+
+    }
+
+    public void bewegungsdaten() {
+
+    }
+
+    public void einsteigen() {
+
+    }
+
+    public void parken() {
+
+    }
+}
